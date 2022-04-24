715d4da915ab603604bb60d8f6b462acc8fc6487
Saqlien-Shareef <saqlienshareef@gmail.com>
2022-04-24 15:26:58 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,38 @@
+var main = document.getElementById("main");
+console.log(main.childNodes);
+
+var maintwo = document.getElementById("main");
+console.log(maintwo.children);
+
+var mainthree = document.getElementById("main");
+console.log(mainthree.firstChild);
+
+var mainfour = document.getElementById("main");
+console.log(mainfour.lastChild);
+
+var mainfive = document.getElementById("main");
+console.log (mainfive.firstElementChild);
+
+var mainsix = document.getElementById("main");
+console.log (mainsix.lastElementChild);
+
+var mainseven = document.getElementById("main");
+console.log (mainseven.parentNode);
+
+var maineight = document.getElementById("main");
+console.log (maineight.parentElement);
+
+var mainnine = document.getElementById("main");
+console.log (mainnine.nextSibling);
+
+var mainele = document.getElementById("main");
+console.log (mainele.previousSibling);
+
+var main = document.getElementById("main")
+console.log (main.childElementCount);
+
+var main = document.getElementById("three")
+console.log (main.previousElementSibling);
+
+var main = document.getElementById("one")
+console.log (main.nextElementSibling);
